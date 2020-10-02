b20435400bb798f7b8672951be61d45fce2532fc
Annetta98 <67956192+Annetta98@users.noreply.github.com>
2020-10-02 16:34:35 +0500

    Add files via upload

---
+++ task.js
@@ -0,0 +1,21 @@
+function averageMark(a, g, p) {
+    let algebra = a;
+    let geography = g;
+    let physics = p;
+     return ((a + g + p)/3);
+}
+
+function sayHello(userName) {
+    let myName = userName;
+    let text = 'Привет, мир! Меня зовут ';
+    let message = text + myName;
+    return message;
+   }
+
+function calculateFormula(x,y,z) {
+    let x = 2;
+    let y = 22;
+    let z = 0;
+    let result = (x * y) + (5 * z) + x - 1;
+        return result;
+}
